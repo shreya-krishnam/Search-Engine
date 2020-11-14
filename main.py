27e0a332694c136f9444aef6565d4af4ecca479a
Shreya Krishnam <50132995+shreya-krishnam@users.noreply.github.com>
2020-11-13 22:27:22 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import json
+import sys
 from bs4 import BeautifulSoup as bs
 import nltk,re
+import pickle
 from nltk.stem import PorterStemmer
-from sklearn.
+from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import defaultdict 
 
 def get_content_string(rawcontent):
     soup = bs(rawcontent, 'html.parser')
@@ -39,27 +42,69 @@ def return_stems(tokens):
         tokens[i] = porter.stem(tokens[i])
     return tokens
 
-def get_tfidf_score(tokens):
+def get_token_occurances(stems):
+    numWords = dict()
+    for word in stems:
+        if word in numWords:
+            numWords[word] += 1
+        else:
+            numWords[word] = 1
+    return numWords
 
+def initialize_posting():
+    obj = open("posting.pickle","wb")
+    posting = defaultdict(defaultdict)
+    pickle.dump(posting,obj)
+    obj.close()
 
+def make_postings(docID,token_count):
+    obj = open("posting.pickle","rb")
+    key_posting = pickle.load(obj)
+    for token,count in token_count.items():
+        key_posting[token][docID] =  count
+    obj.close()
+    obj = open("posting.pickle","wb")
+    pickle.dump(key_posting,obj)
+    obj.close()
 
+def print_report_info():
+    obj = open("posting.pickle","rb")
+    final_key_posting = pickle.load(obj)
+    print(final_key_posting)
+    print("\nNumber of unique words: ",len(final_key_posting))
+    print("\n Size of key posting: ",sys.getsizeof(final_key_posting))
 
 def read_files():
     directory = "C:\\Users\\Shreya\\Documents\\CLASS NOTES\\CS 121\\Assignment3\\developer\\developer\\DEV"
+    initialize_posting()
+    list_of_all_urls =  []
     sub_directories = os.listdir(directory)
-    count = 0
+    docID = 0
     for sub_directory in sub_directories:
         subdirectory_path = os.path.join(directory,sub_directory)
         for file_path in os.listdir(subdirectory_path):
             file_path = os.path.join(subdirectory_path,file_path)
             file_object = open(file_path,"r")
             json_data = json.load(file_object)
-            content_string = get_content_string(json_data["content"])
-            print(tokenize(content_string))
-            count+=1
-            if count>=1:
-                break
-            file_object.close()
+            if json_data["url"] not in list_of_all_urls:
+                docID+=1
+                content_string = get_content_string(json_data["content"])
+                tokens = tokenize(content_string)
+                stems = return_stems(tokens)
+                token_count = get_token_occurances(stems)
+                make_postings(docID,token_count)
+                list_of_all_urls.append(json_data["url"])
+                file_object.close()
+    print_report_info()
+    
+
+
+
+
+
+
+
+
 
 
 
