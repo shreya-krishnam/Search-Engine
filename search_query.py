53aa71e632bd8d973ce93b8d0ff9f8df5761a404
Shreya Krishnam <50132995+shreya-krishnam@users.noreply.github.com>
2020-11-19 21:38:31 -0800

    Add files via upload

--- search_query.py
+++ search_query.py
@@ -21,45 +21,35 @@ def get_docIDs_intersection(query_stems):
         stem_docIDs.append(key_postings[stem].keys())
     return list(reduce(lambda i, j: i & j, (set(x) for x in stem_docIDs))) 
 
-def get_intersected_docIDs_freq_tuple_list(key_postings, query_stems, intersected_docIDs):
-    common_docIDs_freq = []
-    for stem in query_stems:
-        for docID in intersected_docIDs:
-            common_docIDs_freq.append((stem,docID,key_postings[stem][docID]))
-    return sorted(common_docIDs_freq, key = lambda x: (x[0],-x[2],x[1]) )
+def get_relevance_score(intersected_docIDs,key_postings,query_stems):
+    relevance_score_dict = indexer.defaultdict(int)
+    for docID in intersected_docIDs:
+        score = 0
+        for i in range(0,len(query_stems)):
+            score += key_postings[query_stems[i]][docID]
+        relevance_score_dict[docID] = score
+    return sorted(relevance_score_dict.items(), key = lambda x:-x[1])
 
+def get_docid_url():
+    obj = open("doc_id_urls.pickle","rb+")
+    docid_url_dict = indexer.pickle.load(obj)
+    obj.close()
+    return docid_url_dict 
+    
 
-        
-#{token:{docIDs:frequency}}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def print_top_five_urls(sorted_relevance_score):
+    if len(sorted_relevance_score)==0:
+        print("No URL matches found")
+    else:
+        docid_url_dict =  get_docid_url()
+        if 1<=len(sorted_relevance_score)<=4:
+            print("Top",len(sorted_relevance_score),"URL matches found are:")
+            for i in range(0,len(sorted_relevance_score)):
+                print(docid_url_dict[sorted_relevance_score[i][0]])
+        else:
+            print("Top 5 URL matches found are:")
+            for i in range(0,5):
+                print(docid_url_dict[sorted_relevance_score[i][0]])
 
 
 if __name__ == "__main__":
@@ -67,7 +57,5 @@ if __name__ == "__main__":
     query_stems = get_query_stems(user_query)
     key_postings = read_key_postings()
     intersected_docIDs = get_docIDs_intersection(query_stems)
-    docID_freq_list =  get_intersected_docIDs_freq_tuple_list(key_postings, 
-            query_stems, intersected_docIDs)
-    #print(docID_freq_list)
-    print(sorted(key_postings['learn'].items(),key = lambda x: -x[1]))
+    sorted_relevance_score = get_relevance_score(intersected_docIDs,key_postings,query_stems)
+    print_top_five_urls(sorted_relevance_score)
