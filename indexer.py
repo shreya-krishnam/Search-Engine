72164e1222df0599b08e9588328417fa3186e19c
Shreya Krishnam <50132995+shreya-krishnam@users.noreply.github.com>
2020-12-03 20:33:59 -0800

    Add files via upload

--- indexer.py
+++ indexer.py
@@ -6,21 +6,30 @@ import nltk
 import re
 import pickle
 from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import defaultdict 
+import numpy as np
+
+def get_important_words(soup):
+    imp_words = soup.find_all(['title','h1','h2','h3'])
+    imp_text = [tag.get_text() for tag in imp_words]
+    final_imp_word_list = []
+    if imp_text!=[]:
+        for text in imp_text:
+            if text!='':
+                final_imp_word_list.extend([w for w in tokenize(text) if w not in stopwords.words('english')])
+    return return_stems(list(set(final_imp_word_list)))
 
 def get_content_string(rawcontent):
     soup = bs(rawcontent, 'html.parser')
-    text = soup.find_all(text=True)
-    content = ''
-    blacklist = ['style', 'script', 'head',  'meta', '[document]']
-    for t in text:
-        if t.parent.name not in blacklist:
-            content += '{} '.format(t)
-    return content
+    imp_words = get_important_words(soup)
+    text = soup.get_text()
+    return imp_words,text
+
 
 def tokenize(content_string):
-    s = re.compile("[a-z0-9']+")
+    s = re.compile("[a-z0-9]+")
     return re.findall(s,content_string.lower())
 
 def return_stems(tokens):
@@ -38,9 +47,12 @@ def get_token_occurances(stems):
             numWords[word] = 1
     return numWords
 
-def make_postings(docID,token_count,key_posting):
+def make_postings(docID,token_count,key_posting,imp_words):
     for token,count in token_count.items():
-        key_posting[token][docID] =  count
+        if token in imp_words:
+            key_posting[token][docID] =  (count,'y')
+        else:
+            key_posting[token][docID] =  (count,'n')
     return key_posting
 
 def pickle_final_postings(key_posting):
@@ -71,26 +83,15 @@ def pickle_docid_urls(docid_url_dict):
     pickle.dump(docid_url_dict,obj)
     obj.close()
 
-def make_alphabet_number_initial_postings(partial_posting):
-    alphabet_list = [chr(x) for x in range(ord('a'), ord('z') + 1)] 
-    a_partial,b_partial,c_partial,d_partial,e_partial,f_partial,g_partial,h_partial, i_partial, j_partial,k_partial,l_partial,m_partial,n_partial,o_partial,p_partial,q_partial,r_partial,s_partial,t_partial,u_partial,v_partial,w_partial,x_partial,y_partial,z_partial = (defaultdict(defaultdict),)*26
-    num_partial = defaultdict(defaultdict)
-    for k,v in partial_posting.items():
-        if k[0] in alphabet_list:
-            exec_string = k[0].lower()+"_partial[k] = v"
-            exec(exec_string)
-        else:
-            num_partial[k] = v
+def make_alphabet_number_initial_postings():
+    alphabet_list = alphabet_list = [chr(x) for x in range(ord('a'), ord('z') + 1)] 
     for c in alphabet_list:
-        file_path = "partials/"+c+".pickle"
-        obj = open(file_path,"wb")
-        writing_exec_string = "pickle.dump(" +c+ "_partial,obj)"
-        exec(writing_exec_string)
-        obj.close()
+        file_obj = open("partials/"+c+".pickle","wb")
+        pickle.dump(defaultdict(defaultdict),file_obj)
+        file_obj.close()
     num_obj = open("partials/numbers.pickle","wb")
-    pickle.dump(num_partial,num_obj)
+    pickle.dump(defaultdict(defaultdict),num_obj)
     num_obj.close()
-
 
 def merge_and_write_alphabet_number_postings(partial_posting):
     alphabet_list = [chr(x) for x in range(ord('a'), ord('z') + 1)] 
@@ -130,7 +131,6 @@ def merge_and_write_alphabet_number_postings(partial_posting):
     for k,v in sorted_posting:
         sorted_final_posting[k] = v
     alphabet_num_postings['num'] =  sorted_final_posting
-    
     #Writing to disk
     for c in alphabet_list:
         file_obj = open("partials/"+c+".pickle","wb")
@@ -139,13 +139,44 @@ def merge_and_write_alphabet_number_postings(partial_posting):
     num_obj = open("partials/numbers.pickle","wb")
     pickle.dump(alphabet_num_postings['num'],num_obj)
     num_obj.close()
-    
 def create_alphabetical_order_posting(partial_obj,partial_number):
     partial_posting = pickle.load(partial_obj)
     if partial_number == 1:
-        make_alphabet_number_initial_postings(partial_posting)
-    else:
-       merge_and_write_alphabet_number_postings(partial_posting) 
+        make_alphabet_number_initial_postings()
+    merge_and_write_alphabet_number_postings(partial_posting) 
+
+def calculate_tfidf(tf,N,df):
+    if tf==0 or df==0:return 0
+    return (1+np.log10(tf))*(np.log10(N/df))
+
+def write_tfidf(N):
+    alphabet_list = [chr(x) for x in range(ord('a'), ord('z') + 1)] 
+    alphabet_num_postings = dict()
+    for c in alphabet_list: #opening all files
+        file_obj = open("partials/"+c+".pickle","rb")
+        alphabet_num_postings[c] = pickle.load(file_obj)
+        file_obj.close()
+    num_obj = open("partials/numbers.pickle","rb")
+    alphabet_num_postings['num'] = pickle.load(num_obj)
+    num_obj.close()
+    for c,v in alphabet_num_postings.items():
+        for term,posting in v.items():
+            for docID,tf_tuple in posting.items():
+                tfidf_score = calculate_tfidf(tf_tuple[0],N,len(posting))
+                if tf_tuple[1] == 'y':
+                    tfidf_score += 0.25 * tfidf_score 
+                alphabet_num_postings[c][term][docID] = tfidf_score
+            sorted_tfidf = {docID: tfidf for docID,tfidf in sorted(alphabet_num_postings[c][term].items(),key = lambda kv:(-kv[1],kv[0]))}
+            sorted_tfidf = defaultdict(float, sorted_tfidf)
+            alphabet_num_postings[c][term] = sorted_tfidf
+    #write
+    for c in alphabet_list:
+        file_obj = open("partials_tfidf/"+c+".pickle","wb")
+        alphabet_num_postings[c] = pickle.dump(alphabet_num_postings[c],file_obj)
+        file_obj.close()
+    num_obj = open("partials_tfidf/numbers.pickle","wb")
+    pickle.dump(alphabet_num_postings['num'],num_obj)
+    num_obj.close()
 
 def read_files():
     directory = "C:\\Users\\Shreya\\Documents\\CLASS NOTES\\CS 121\\Assignment3\\developer\\developer\\DEV"
@@ -162,17 +193,19 @@ def read_files():
             json_data = json.load(file_object)
             if json_data["url"] not in list_of_all_urls:
                 docID+=1
-                content_string = get_content_string(json_data["content"])
+                print(docID)
+                imp_words,content_string = get_content_string(json_data["content"])
                 tokens = tokenize(content_string)
                 stems = return_stems(tokens)
                 token_count = get_token_occurances(stems)
-                key_posting = make_postings(docID,token_count,key_posting)
+                key_posting = make_postings(docID,token_count,key_posting,imp_words)
                 list_of_all_urls.append(json_data["url"])
                 docid_url_dict = make_dict_urls(docid_url_dict,docID,json_data["url"])
                 file_object.close()
                 if docID % 15000 == 0:
                     sortandwritetodisk(key_posting,docID//15000)
                     key_posting.clear()
+    N = 55393
     sortandwritetodisk(key_posting,(docID//15000)+1)
     partial1_obj = open("partial1.pickle","rb")
     partial2_obj = open("partial2.pickle","rb")
@@ -188,6 +221,8 @@ def read_files():
     partial2_obj.close()
     partial3_obj.close()
     partial4_obj.close()
+
+    write_tfidf(N)
 
 if __name__ == "__main__":
     read_files()
